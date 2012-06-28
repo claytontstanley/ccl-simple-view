774f6b3a19a9f9492077aebf0d4de1ef28a98c75
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-06-28 17:06:56 -0500

    ran through unit tests

--- testing/testSound.lisp
+++ testing/testSound.lisp
@@ -1,4 +1,5 @@
-
+; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
+(load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
 (setf *pool* (init-pool))
 
