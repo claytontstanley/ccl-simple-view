55c72726bfe396723668e74c0bef72419625cf50
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-07-10 17:20:32 -0500

    added file-list for votebox

--- testing/testVotebox.lisp
+++ testing/testVotebox.lisp
@@ -1,14 +1,8 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 #-:ccl-simple-view (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
-(load-as-lst "votebox" "Virtual Cursor" "osx-virtual-cursor.lisp")
-(load-as-lst "bincarbon" "misc-lib.lisp")
-
-(load-as-lst "bincarbon" "pict-svm.lisp")
-
-(load-as-lst "votebox" "Emulator Files" "VBEmulator.lisp")
-
-(load-as-lst "votebox" "VB Models" "Bryan PSYC 543 Project (2009)" "BryansVBModel (Navigation + Selection - Best Fit).lisp")
+(dolist (file (file-lines (path-as-lst "file-lists" "testVotebox.txt")))
+  (load (path-as-lst file)))
 
 (do-experiment wind)
 
