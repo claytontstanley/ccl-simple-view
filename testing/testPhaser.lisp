1495e17fc0a4e88d727763440053568cec0e2d41
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-07-11 12:58:52 -0500

    Updated Phaser to use file-lists

--- testing/testPhaser.lisp
+++ testing/testPhaser.lisp
@@ -1,19 +1,10 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 #-:ccl-simple-view (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
-;(setf easygui::*debug-cocoa-calls* t)
-
-(load-in-bincarbon "misc-lib.lisp" "timer.lisp" "snd-player.lisp" "pict-svm.lisp" "procedure-window2.lisp" "base-trek-tasks.lisp")
-;(provide :timer)
-;(load-in-bincarbon "snd-player.lisp" "pict-svm.lisp" "misc-lib.lisp" "procedure-window2.lisp" "base-trek-tasks.lisp")
-
-;(load-in-bincarbon "CFBundle.lisp" "seq-math.lisp" "launch-url.lisp" "experiment-window4.lisp")
+; FIXME: Does MCL need CFBundle.lisp to run Phaser?
 (provide :cfbundle)
-(load-in-bincarbon "seq-math.lisp" "launch-url.lisp" "experiment-window4.lisp")
 
-(load-in-bincarbon "blinker.lisp" "menubar-hide.lisp")
-
-(load-as-lst "phaser" "X83.lisp")
+(load-file-list "file-lists" "Phaser" "testPhaser.txt")
 
 ;(get-string-from-user "hello")
 ;
