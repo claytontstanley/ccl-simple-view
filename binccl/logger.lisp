d910efea4f16bde7cc446556af51ca837af7e173
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-08-03 13:42:45 -0500

    added fresh-line call to logger

--- binccl/logger.lisp
+++ binccl/logger.lisp
@@ -2,5 +2,6 @@
   (multiple-value-bind (second minute hour date month year day-of-week dst-p tz) (get-decoded-time)
     (declare (ignore tz dst-p day-of-week))
     (format t "sv-log: ~a-~2,'0d-~2,'0d_~2,'0d:~2,'0d:~2,'0d: " year month date hour minute second))
-  (apply #'format t args))
+  (unwind-protect (apply #'format t args)
+    (fresh-line t)))
 
