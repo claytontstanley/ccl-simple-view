b1631aba175d1c4ea917a0cacb1ea50307f32910
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-08-16 11:24:44 -0500

    melissas code runs on rmcl

---
+++ testing/testGallagher.lisp
@@ -0,0 +1,10 @@
+; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
+(load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
+
+; FIXME: Does MCL need CFBundle.lisp to run Phaser?
+(provide :cfbundle)
+
+(load-file-list "file-lists" "Gallagher" "testGallagher.txt")
+
+(begin-experiment)
+
