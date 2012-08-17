4e7a8e9b5c7b4cf1c0fd19c3134a8026be5791aa
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2012-08-17 18:24:26 -0500

    updated polygon code

--- testing/testPolygon.lisp
+++ testing/testPolygon.lisp
@@ -1,20 +1,21 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
-(setf *win* (make-instance 'window))
+(defparameter *win* (make-instance 'window))
 
-(setf *view* (make-instance 'simple-view
+(defparameter *view* (make-instance 'simple-view
                             :view-size (make-point 50 50)))
+
+(defmethod view-draw-contents ((view (eql *view*))) 
+  (start-polygon *view*)
+  (move-to view 0 0)
+  (line-to view 0 10)
+  (line-to view 10 10)
+  (line-to view 10 0)
+  (line-to view 0 0)
+  (let ((polygon (get-polygon view)))
+    (fill-polygon view *black-pattern* polygon)
+    (kill-polygon polygon)))
 
 (add-subviews *win* *view*)
 
-(move-to *view* 0 0)
-
-(start-polygon *view*)
-
-(line-to *view* 0 10)
-(line-to *view* 10 10)
-(line-to *view* 10 0)
-(line-to *view* 0 0)
-
-(fill-polygon *view* *black-pattern* nil)
