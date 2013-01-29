bfcf2d6b402f4c73e5b4ebf7181b208cfba5013c
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-01-29 17:56:51 -0600

    nsfmotor seems to work fine

---
+++ testing/testNSFMotor.lisp
@@ -0,0 +1,10 @@
+; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
+(load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
+
+(load-file-list "file-lists" "NSFMotor" "testNSFMotor.txt")
+
+(setf *exp* (make-instance 'target-window))
+
+(run-model)
+
+
