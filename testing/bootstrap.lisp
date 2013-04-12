fd20bc5cb2c95dfa6ffde1e7f9a75ce0aa49ecf4
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-04-11 21:12:54 -0500

    foo

--- testing/bootstrap.lisp
+++ testing/bootstrap.lisp
@@ -4,21 +4,20 @@
 
 (load (format nil "~a~a" (directory-namestring *load-truename*) "utilities.lisp"))
 
-(defparameter *base-repo-namestring*
-  (format nil "~a~a~a" (directory-namestring *load-truename*) ".." *path-separator*))
-
 (defun load-file-list (&rest file-list)
   "Loads each file in file-list, where the location of 
    file-list is a relative path from the load file's directory
    and the location of each file in file-list is a relative path from
    the base repo's directory"
-  (dolist (file (file-lines (apply #'path-as-lst file-list)))
-    (let ((file (replace-all file "/" *path-separator*)))
-      (let ((file (format nil "~a~a" *base-repo-namestring* file)))
-        (cond ((search "load-act-r-6.lisp" file)
-               #-:act-r-6.0 (load file))
-              (t
-               (load file)))))))
+  (let ((base-repo-namestring
+          (format nil "~a~a~a" (directory-namestring *load-truename*) ".." *path-separator*)))
+    (dolist (file (file-lines (apply #'path-as-lst file-list)))
+      (let ((file (replace-all file "/" *path-separator*)))
+        (let ((file (format nil "~a~a" base-repo-namestring file)))
+          (cond ((search "load-act-r-6.lisp" file)
+                 #-:act-r-6.0 (load file))
+                (t
+                 (load file))))))))
 
 #-:act-r-6.0 (load-as-lst ".." "submodules" "actr6" "load-act-r-6.lisp")
 
@@ -32,7 +31,7 @@
 
 #+:clozure (setf *resource-pool* (init-pool))
 
-(load-file-list "file-lists" "all.txt")
+(load-as-lst ".." "submodules" "lisp-dev" "Lisp-Unit-Testing-Framework" "unitTestFramework.lisp")
 
 #+:digitool (load-as-lst ".." "bincarbon" "bootstrap-mcl.lisp")
 
