df81fe2a763761528f94fe4e17940e50a51f39ef
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-05-08 22:31:35 -0500

    Allowing quit

--- testing/testStanleyThesis.lisp
+++ testing/testStanleyThesis.lisp
@@ -6,10 +6,15 @@
 #|
 (begin-experiment
   :eyetracking nil
-  :allowing-quit nil
+  :allowing-quit t
   :short-stack 20)
 
 (subviews *library-experiment-window*)
 (show-cursor)
-
+(make-instance 'window)
+(inspect *)
+(write-readable (front-window) *)
+(write-readable *library-experiment-window* t)
+(inspect *library-experiment-window*)
+*features*
 |#
