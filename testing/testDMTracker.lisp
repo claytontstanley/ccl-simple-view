69872c464c6f1daa525436c3f42102709621d912
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-05-10 15:39:09 -0500

    Added build for DMTracker

--- testing/testDMTracker.lisp
+++ testing/testDMTracker.lisp
@@ -1,7 +1,7 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
-(load-as-lst ".." "bincarbon" "DMTracker-bootstrap.lisp")
+(load-file-list "file-lists" "DMTracker" "testDMTracker.txt")
 
 #|
 (require :tracker-experiment-window)
