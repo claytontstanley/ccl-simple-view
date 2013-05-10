18bfb15231f88037ae46123321ded33cd69a87a7
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-05-10 18:14:29 -0500

    Added initial unit framework

---
+++ testing/testUnit.lisp
@@ -0,0 +1,6 @@
+; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
+(load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
+
+; Don't reload ccl-simple-view code anymore; already loaded.
+(let ((*load-sv-dev-files-p* nil))
+  (load-file-list "file-lists" "Unit" "testUnit.txt"))
