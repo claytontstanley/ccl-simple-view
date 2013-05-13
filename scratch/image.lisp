3f53f400157dccc50894a100a0fe6d9daa7181df
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-05-12 21:01:12 -0500

    Not tracking scratch folder

--- scratch/image.lisp
+++
@@ -1,9 +0,0 @@
-
-(defclass dummy (image-view-mixin window) ())
-(inspect 'initialize-instance)
-(find-method #'initialize-instance '(:after) '(image-view))
-(remove-method #'initialize-instance *)
-(make-instance 'dummy
-               :view-position #@(100 100)
-               :view-size #@(400 400))
-(class-default-initargs (find-class 'dummy))
