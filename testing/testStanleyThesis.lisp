899e5ef6a635b08b7d37f41f2ef90516de4e96f1
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-05-28 23:17:50 -0500

    Debugging tracker data

--- testing/testStanleyThesis.lisp
+++ testing/testStanleyThesis.lisp
@@ -6,18 +6,22 @@
 ; opens a window, sets contents to *library-experiment-window*, and closes window when done
 (let ((*path* (format nil "~a/" (path-as-lst ".." "submodules" "stanley-thesis" "participant data" "eye-tracked participants" "source"))))
   (things2lisp :snums (list 50)))
+(break)
+
+(defmethod play-expt-with-window ((window window) &optional (constraints))
+  (let ((prop (make-instance 'analysis-properties))) 
+    (play-expt :prop prop
+               :win window 
+               :constraints constraints)))
+    (window-close
+      (replay-win
+        (replay-mod prop)))))
 
 ; *library-experiment-window* is currently set to subject 50 source lisp file
-(let ((prop (make-instance 'analysis-properties))) 
-  (play-expt :prop prop
-             :win *library-experiment-window*
-             :constraints (list
-                            (make-instance 'trial-constraint :index 7)
-                            (make-instance 'trial-constraint :index 9)
-                            ))
-  (window-close
-    (replay-win
-      (replay-mod prop))))
+(play-expt-with-window *library-experiment-window*
+                       (list 
+                         (make-instance 'trial-constraint :index 7)
+                         (make-instance 'trial-constraint :index 9)))
 
 ; overwrites *library-experiment-window* with subject 50 combined file; just checking that this file can be read in
 ; opens *library-experiment-window*
@@ -27,9 +31,15 @@
 (window-close *library-experiment-window*)
 #|
 
-(let ((*path* "/Volumes/claytonstanley/Desktop/")) 
-  (things2lisp :snums (list 2)))
 
+(let ((*path* "/Volumes/claytonstanley/Desktop/")) 
+  (let ((*library-experiment-window*))
+    (things2lisp :snums (list 2))
+    (setf *library-experiment-window-2* *library-experiment-window*)))
+
+(let ((*library-experiment-window* *library-experiment-window-2*))
+  (play-expt-with-window *library-experiment-window*))
+  
 (load-as-lst ".." "submodules" "stanley-thesis" "participant data" "eye-tracked participants" "source" "subj050.lisp")
 
 (with-open-file (strm "foo.lisp" :direction :output :if-exists :supersede) 
