6e7dd36da2e4882cc900e4e72bfd9a6e7c368f33
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-06-25 22:43:34 -0500

    Added a few tests in dev environment config

--- check-dev-env.sh
+++ check-dev-env.sh
@@ -4,17 +4,37 @@ echo $BASH_VERSION
 
 checkDependency() {
 	if eval "$1"; then 
-		echo "$2"
-		exit 1
-	else
 		echo "Passed test: $1"
+	else
+		printf "$2\n"
+		exit 1
 	fi
 }
 
-checkDependency "[[ -z '$(which make)' ]]" "Install make (suggestion: Install XCode and check the box to install command line tools)"
-checkDependency "[[ -z '$(which port)' ]]" "Install MacPorts (suggestion: Install using the binary found on MacPorts webpage)"
-checkDependency "[[ -z '$(which greadlink)' ]]" "Install greadlink (suggestion: sudo port install coreutils)"
-checkDependency "[[ -z '$(which rlwrap)' ]]" "Install rlwrap (suggestion: sudo port install rlwrap)"
+checkNoPassword() {
+	ip=$1
+	status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $ip echo ok 2>&1)
+	[[ $status == ok ]]
+}
+
+checkSSHHostname() {
+	egrep -iq "^Host[[:space:]]+$1$" ~/.ssh/config
+}
+
+hostnameTemplate=$(cat <<'EOF'
+Host chil
+	User		clayton	
+	HostName 	chil.rice.edu
+EOF
+)
+
+checkDependency "[[ -n '$(which make)' ]]" "Install make (suggestion: Install XCode and check the box to install command line tools)"
+checkDependency "[[ -n '$(which port)' ]]" "Install MacPorts (suggestion: Install using the binary found on MacPorts webpage)"
+checkDependency "[[ -n '$(which greadlink)' ]]" "Install greadlink (suggestion: sudo port install coreutils)"
+checkDependency "[[ -n '$(which rlwrap)' ]]" "Install rlwrap (suggestion: sudo port install rlwrap)"
+checkDependency "[[ -n '$(which ssh-copy-id)' ]]" "Install ssh-copy-id (suggestion: sudo port install openssh)"
+checkDependency "checkSSHHostname chil" "chil hostname not configured: add something like this to ~/.ssh/config: \n$hostnameTemplate"
+checkDependency "checkNoPassword chil" "chil not good: use ssh-copy-id to enable passwordless login to chil using public/private keys; otherwise submodule updating gets tedious"
 
 echo "Initializing and updating submodules"
 git submodule update --init --recursive
