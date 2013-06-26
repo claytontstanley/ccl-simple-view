888c6d5b76a37cf4f9d26573e3a493c45c811271
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-06-25 22:45:37 -0500

    foo

--- check-dev-env.sh
+++ check-dev-env.sh
@@ -31,7 +31,7 @@ checkDependency "[[ -n '$(which port)' ]]" "Install MacPorts (suggestion: Instal
 checkDependency "[[ -n '$(which greadlink)' ]]" "Install greadlink (suggestion: sudo port install coreutils)"
 checkDependency "[[ -n '$(which rlwrap)' ]]" "Install rlwrap (suggestion: sudo port install rlwrap)"
 checkDependency "[[ -n '$(which ssh-copy-id)' ]]" "Install ssh-copy-id (suggestion: sudo port install openssh)"
-checkDependency "checkSSHHostname chil" "chil hostname not configured: add something like this to ~/.ssh/config: \n$hostnameTemplate"
+checkDependency "checkSSHHostname chil" "chil hostname not configured: add something like this (replace User field with your chil login name) to ~/.ssh/config: \n$hostnameTemplate"
 checkDependency "checkNoPassword chil" "chil not good: use ssh-copy-id to enable passwordless login to chil using public/private keys; otherwise submodule updating gets tedious"
 
 echo "Initializing and updating submodules"
