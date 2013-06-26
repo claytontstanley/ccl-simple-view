82cff079413f9eb9dde65cc4d0ec0d28450dec53
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-06-26 16:45:16 -0500

    foo

--- fonts/fix-attributes.sh
+++ fonts/fix-attributes.sh
@@ -6,4 +6,4 @@ AG="46 46 49 4C 44 4D 4F 56 01 40 FF FF FF FF 00 00
 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
 
 xattr -wx com.apple.FinderInfo "$AG" "$pathToThisDir/Avant Garde"
-xxd -r -p "$pathToThisDir/Avant Garde rsrcfork.txt" >  "$pathToThisDir/Avant Garde/..namedfork/rsrc"
+xxd -r -p "$pathToThisDir/Avant Garde rsrcfork.txt" > "$pathToThisDir/Avant Garde/..namedfork/rsrc"
