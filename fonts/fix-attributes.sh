27dc78988e3525d87140ebba5f33aedca1bc8832
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-06-26 23:11:42 -0500

    Refactor

--- fonts/fix-attributes.sh
+++ fonts/fix-attributes.sh
@@ -8,4 +8,4 @@ AG="46 46 49 4C 44 4D 4F 56 01 40 FF FF FF FF 00 00
 xattr -wx com.apple.FinderInfo "$AG" "$pathToThisDir/Avant Garde"
 
 #ref: http://stackoverflow.com/questions/13533796/copy-mac-com-apple-resourcefork-extended-attribute-causing-argument-list-too-lo
-xxd -r -p "$pathToThisDir/Avant Garde rsrcfork.txt" > "$pathToThisDir/Avant Garde/..namedfork/rsrc"
+xxd -r -p "$pathToThisDir/AvantGardeRsrcFork.hex" > "$pathToThisDir/Avant Garde/..namedfork/rsrc"
