f3a5f2abb0cc66030ee08a390ee6fa43bb9cc10b
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-08-05 21:17:13 -0500

    Added more coverage

--- testing/testGetText.lisp
+++ testing/testGetText.lisp
@@ -1,30 +1,46 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
-(process-run-function
-  "foo"
-  (lambda ()
-    (sleep 1)
-    (left-mouse-click (view-position (front-window)))
-    (sleep .3)
-    (keypress #\a)
-    (sleep .3)
-    (left-mouse-click
-      (add-points
-        (view-position (front-window))
-        (view-center
-          (first (remove-if-not
-                   (lambda (x)
-                     (equalp (class-name (class-of x))
-                             'default-button-dialog-item))
-                   (subviews (front-window)))))))))
+(progn
+  (process-run-function
+    "foo"
+    (lambda ()
+      (sleep 1)
+      (left-mouse-click (view-position (front-window)))
+      (let ((cancel
+              (first (remove-if-not
+                       (lambda (x)
+                         (string-equal "cancel" (dialog-item-text x)))
+                       (view-subviews (front-window))))))
+        (left-mouse-click (local-to-global (front-window) (view-center cancel))))))
+  (setf *str* (get-string-from-user
+                "prompt"
+                :position (list :left 0)))
+  (check (eq *str* :cancel)))
 
-(setf *str* (get-string-from-user
-              "prompt"
-              :position (make-point 10 10)
-              :initial-string "foo"))
-
-(check (string= *str* "a"))
+(progn
+  (process-run-function
+    "foo"
+    (lambda ()
+      (sleep 1)
+      (left-mouse-click (view-position (front-window)))
+      (sleep .3)
+      (keypress #\a)
+      (sleep .3)
+      (left-mouse-click
+        (add-points
+          (view-position (front-window))
+          (view-center
+            (first (remove-if-not
+                     (lambda (x)
+                       (equalp (class-name (class-of x))
+                               'default-button-dialog-item))
+                     (subviews (front-window)))))))))
+  (setf *str* (get-string-from-user
+                "prompt"
+                :position (make-point 10 10)
+                :initial-string "foo"))
+  (check (string= *str* "a")))
 
 
 
