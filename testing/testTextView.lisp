1c582295efc4e2f8d8a465dcd78879eefa0c49c4
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-09-05 21:25:19 -0500

    Added test for text view

---
+++ testing/testTextView.lisp
@@ -0,0 +1,25 @@
+(make-instance
+  'window
+  :view-subviews
+  (list
+    (make-instance
+      'text-view
+      :view-nick-name :foo
+      :view-size (make-point 100 50))))
+
+(left-mouse-click (view-position (front-window)))
+(left-mouse-click (add-points
+                    (view-position (front-window))
+                    (view-center (view-named :foo (front-window)))))
+
+(sleep .5)
+(progn
+  (keypress #\a)
+  (keypress #\return)
+  (keypress-on-view (view-named :foo (front-window)) (format nil "~%"))
+  (keypress-on-view (view-named :foo (front-window)) #\newline)
+  (keypress #\b)
+  )
+
+(check (string-equal (dialog-item-text (view-named :foo (front-window)))
+                     (format nil "a~%~%~%b")))
