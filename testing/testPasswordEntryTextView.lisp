06e429dd4d094a43130edc284c8783927bfbabea
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-09-12 21:48:42 -0500

    removed references to text-view.lisp

--- testing/testPasswordEntryTextView.lisp
+++ testing/testPasswordEntryTextView.lisp
@@ -1,8 +1,8 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
-(load-as-lst ".." "bincarbon" "text-view.lisp")
 (load-as-lst ".." "bincarbon" "chil-ccl-utilities.lisp")
+(load-as-lst ".." "bincarbon" "direct-event-calls.lisp")
 (load-as-lst ".." "bincarbon" "password-entry-text-view.lisp")
 
 (setf *win*
