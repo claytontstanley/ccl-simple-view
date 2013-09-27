905b246c994af451dcba94f49ff423f8b6a36174
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-09-27 13:03:09 -0500

    su and test update for touch keyboard

--- testing/testTouchKeyboard.lisp
+++ testing/testTouchKeyboard.lisp
@@ -23,6 +23,75 @@
                                   :view-size (make-point (* 80 11) (* 27 11))
                                   ))
   :view-size (make-point (* 80 11) (+ 100 (* 27 11))))
+
+(make-instance
+  'window
+  :view-position (make-point 10 100)
+  :view-subviews (list
+                   (make-instance 'tablet-portrait-touch-keyboard
+                                  :view-nick-name :keyboard
+                                  :initial-layout :sd3
+                                  :view-position (make-point 0 100)
+                                  :view-size (make-point (* 32 19) (* 11 19))
+                                  ))
+  :view-size (make-point (* 32 19) (+ 100 (* 11 19))))
+
+
+(make-instance
+  'window
+  :view-position (make-point 10 100)
+  :view-subviews (list
+                   (make-instance 'tablet-landscape-touch-keyboard
+                                  :view-nick-name :keyboard
+                                  :initial-layout :sd1
+                                  :view-position (make-point 0 100)
+                                  :view-size (make-point (* 32 19) (* 11 19))
+                                  ))
+  :view-size (make-point (* 32 19) (+ 100 (* 11 19))))
+#|
+(remove-keys (view-named :keyboard (front-window)))
+(print-pool)
+(get-resource-val "smartphone-portrait-sd1" 'image)
+(get-resource-val "voteboxbg" 'image)
+(setf (pict-id (view-named :keyboard (front-window))) "smartphone-portrait-sd1")
+(getf (layouts (view-named :keyboard (front-window))) :smartphone-portrait-sd1)
+(get-layout (view-named :keyboard (front-window)) :smartphone-portrait-sd1)
+(subviews (view-named :keyboard (front-window)))
+(#/image (cocoa-ref *))
+(invalidate-view (front-window))
+(unintern 'keys)
+(inspect *)
+(inspect (front-window))
+(setf *resource-pool* (init-pool))
+(print-pool)
+(alloc-resources)
+(open-resource-folder (format nil "~a~a/" *dir* "touch-keyboard-images"))
+(print *dir*)
+|#
+
+
+
+#|
+(remove-keys (view-named :keyboard (front-window)))
+(print-pool)
+(get-resource-val "smartphone-portrait-sd1" 'image)
+(get-resource-val "voteboxbg" 'image)
+(setf (pict-id (view-named :keyboard (front-window))) "smartphone-portrait-sd1")
+(getf (layouts (view-named :keyboard (front-window))) :smartphone-portrait-sd1)
+(get-layout (view-named :keyboard (front-window)) :smartphone-portrait-sd1)
+(subviews (view-named :keyboard (front-window)))
+(#/image (cocoa-ref *))
+(invalidate-view (front-window))
+(unintern 'keys)
+(inspect *)
+(inspect (front-window))
+(setf *resource-pool* (init-pool))
+(print-pool)
+(alloc-resources)
+(open-resource-folder (format nil "~a~a/" *dir* "touch-keyboard-images"))
+(print *dir*)
+|#
+
 #|
 (remove-keys (view-named :keyboard (front-window)))
 (print-pool)
