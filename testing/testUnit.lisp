ce5229bbef5ffc7cadbd6670af75f51714842c32
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2013-10-03 14:22:55 -0500

    reworking last commit

--- testing/testUnit.lisp
+++ testing/testUnit.lisp
@@ -1,6 +1,10 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
+(defmethod load-file :after (file)
+  (mapcar '#/close (ns-array->list (#/windows (#/sharedApplication ns:ns-application)))))
+
 ; Don't reload ccl-simple-view code anymore; already loaded.
 (let ((*load-sv-dev-files-p* nil))
   (load-file-list "file-lists" "Unit" "testUnit.txt"))
+
