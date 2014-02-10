95190853bd08fc74956d792b734634c43c9f0475
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-02-10 15:21:59 -0600

    foo

--- testing/testMessageDialog.lisp
+++ testing/testMessageDialog.lisp
@@ -1,30 +1,46 @@
 ; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
 (load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
 
+(defun delayed-click-dialog ()
+  (let ((win (front-window)))
+    (process-run-function
+      "foo"
+      (lambda ()
+        (while (eq win (front-window))
+          (sleep .1))
+        (left-mouse-click (view-position (front-window)))
+        (let ((button
+                (first
+                  (remove-if-not
+                    (lambda (view)
+                      (inherit-from-p view 'button-dialog-item))
+                    (view-subviews (front-window))))))
+          (left-mouse-click (local-to-global (front-window) (view-center button))))))))
+
+(defun test-message-dialog-custom-lambda ()
+  (let ((did-fire-lambda nil))
+    (delayed-click-dialog)
+    (message-dialog
+      "so"
+      :position (list :left 0)
+      :on-ok-click (lambda (item)
+                     (declare (ignore item))
+                     (setf did-fire-lambda t)
+                     (return-from-modal-dialog t)))
+    (check did-fire-lambda)))
+
+(defun test-message-dialog ()
+  (delayed-click-dialog)
+  (message-dialog "so" :position (list :left 0)))
+
+(dotimes (i 10)
+  (test-message-dialog)
+  (test-message-dialog-custom-lambda))
+
+
 (defun test-message-dialog ()
   (let ((win (front-window)))
     (let ((did-fire-lambda))
-      (process-run-function
-        "foo"
-        (lambda ()
-          (while (eq win (front-window))
-            (sleep .1))
-          (left-mouse-click (view-position (front-window)))
-          (let ((button
-                  (first
-                    (remove-if-not
-                      (lambda (view)
-                        (inherit-from-p view 'button-dialog-item))
-                      (view-subviews (front-window))))))
-            (left-mouse-click (local-to-global (front-window) (view-center button))))))
-      (message-dialog
-        "so"
-        :position (list :left 0)
-        :on-ok-click (lambda (item)
-                       (declare (ignore item))
-                       (setf did-fire-lambda t)
-                       (return-from-modal-dialog t)))
-      (check did-fire-lambda))))
 
 (dotimes (i 10)
   (test-message-dialog))
