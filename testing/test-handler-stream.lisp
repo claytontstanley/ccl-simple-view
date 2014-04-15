778d173f12685bbaf1ca251d41f428631ccfdaa6
Clayton Stanley <cstanley@cstanley.no-ip.biz>
2014-04-15 16:47:43 -0500

    foo

---
+++ testing/test-handler-stream.lisp
@@ -0,0 +1,25 @@
+; Bootstrap all needed packages (loads ACT-R, Cocoa framework, etc.)
+(load (format nil "~a~a" (directory-namestring *load-truename*) "bootstrap.lisp"))
+
+(defclass my-rpm-win (rpm-real-window)
+  ())
+
+(defparameter *so-test* nil)
+
+(defmethod rpm-window-click-event-handler ((win my-rpm-win) (loc t))
+  (check (eq ccl::*initial-process* *current-process*))
+  (setf *so-test* *standard-output*)
+  (beep))
+
+(make-instance 'my-rpm-win)
+(running-on-main-thread ()
+  (let ((*listener-output* *listener-output*))
+    (post-view-click-event-handler (front-window) (make-point 1 1))))
+(check (eq *so-test* *standard-output*))
+(running-on-main-thread ()
+  (let ((*listener-output* *standard-output*))
+    (post-view-click-event-handler (front-window) (make-point 1 1))))
+(check (not (eq *so-test* *standard-output*)))
+
+(window-close (front-window))
+
